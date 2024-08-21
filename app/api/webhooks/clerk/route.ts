fe9c7d0a8019dc8cd8a00173a99a00b3d2013228
abhishekantala24 <127835661+abhishekantala24@users.noreply.github.com>
2024-08-21 17:08:37 +0530

    test

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -64,7 +64,7 @@ export async function POST(req: Request) {
     const user: any = {
       type: UserType.Admin,
       email: email_addresses[0].email_address,
-      name: username!,
+      name: `${username}`,
       profilePicture: image_url,
       password: "93rd398fn",
       phone: "74734783478"
