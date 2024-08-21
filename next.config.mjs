b155545bc480a7a29d68e6f6721877169e632be0
abhishekantala24 <127835661+abhishekantala24@users.noreply.github.com>
2024-08-21 12:52:40 +0530

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ const nextConfig = {
         // MONGODB_URI: "mongodb+srv://dev:iawdjv7qxxpsok32@pro-card-dev.15ue9hq.mongodb.net/?retryWrites=true&w=majority&appName=pro-card-dev",
         NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: "pk_test_YW11c2luZy1zY3VscGluLTEuY2xlcmsuYWNjb3VudHMuZGV2JA",
         CLERK_SECRET_KEY: "sk_test_kkwUH600IR0GNK6pBSISfNMGDZmW2UbL19YfYO6I1V",
+        CLERK_ENCRYPTION_KEY: "sk_test_kkwUH600IR0GNK6pBSISfNMGDZmW2UbL19YfYO6I1V",
         WEBHOOK_SECRET: "whsec_FzkRSjZEJoWPx2vhPqSJKsTtiA+z9v4g",
         // NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: "pk_test_c21hcnQtcm9kZW50LTUzLmNsZXJrLmFjY291bnRzLmRldiQ",
         // CLERK_SECRET_KEY: "sk_test_yfUsFosxREu9n2r7n5gYCvn3WZq8aWo5VNhR7mJwS0",
