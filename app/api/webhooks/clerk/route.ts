8ac29e4e9037a62b21cd47b87753265a76d0e5e8
abhishekantala24 <127835661+abhishekantala24@users.noreply.github.com>
2024-08-21 18:10:56 +0530

    commit

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -80,6 +80,7 @@ export async function POST(req: Request) {
       await clerkClient.users.updateUserMetadata(id, {
         publicMetadata: {
           userId: newUser._id,
+          publicMetadata: { role: UserType.Admin },
         },
       });
     }
