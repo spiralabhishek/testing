e0a4510e1d57cf5c7417fbf3aca059aa4ff0afda
abhishekantala24 <127835661+abhishekantala24@users.noreply.github.com>
2024-08-21 17:11:41 +0530

    commit

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -22,7 +22,7 @@ async function dbConnect() {
     cached.promise =
         cached.promise ||
         mongoose.connect(MONGODB_URL, {
-            dbName: "clerk-next14-db",
+            dbName: "pro-card",
             bufferCommands: false,
             connectTimeoutMS: 30000,
         });
