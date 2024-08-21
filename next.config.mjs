8d3c63b2a24ce87108e5565c6e32502da051dd57
abhishekantala24 <127835661+abhishekantala24@users.noreply.github.com>
2024-08-21 14:47:18 +0530

    work

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ const nextConfig = {
         NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: "pk_test_YW11c2luZy1zY3VscGluLTEuY2xlcmsuYWNjb3VudHMuZGV2JA",
         CLERK_SECRET_KEY: "sk_test_kkwUH600IR0GNK6pBSISfNMGDZmW2UbL19YfYO6I1V",
         CLERK_ENCRYPTION_KEY: "sk_test_kkwUH600IR0GNK6pBSISfNMGDZmW2UbL19YfYO6I1V",
-        WEBHOOK_SECRET: "whsec_FzkRSjZEJoWPx2vhPqSJKsTtiA+z9v4g",
+        WEBHOOK_SECRET: "whsec_t75LmE2qZ8tRjmOyZ0uHK9Z+sWK3zZK9",
         // NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: "pk_test_c21hcnQtcm9kZW50LTUzLmNsZXJrLmFjY291bnRzLmRldiQ",
         // CLERK_SECRET_KEY: "sk_test_yfUsFosxREu9n2r7n5gYCvn3WZq8aWo5VNhR7mJwS0",
     }
