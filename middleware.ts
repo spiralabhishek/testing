0d5e111271ee0a807409f5bbed7b24cae2852940
abhishekantala24 <127835661+abhishekantala24@users.noreply.github.com>
2024-08-23 10:35:59 +0530

    commit

--- middleware.ts
+++ middleware.ts
@@ -25,7 +25,6 @@ export const config = {
   matcher: [
     "/((?!.+\\.[\\w]+$|_next).*)",
     "/",
-    "/(api|trpc)(.*)",
-    "/posts", // add this line to make all /posts routes public
+    "/(api|trpc)(.*)"
   ],
 };
